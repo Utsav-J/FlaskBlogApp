4b96b39557ef1c6bfb2ea6473e53b8baf407051e
Utsav-J <utsavjaiswal2004@gmail.com>
2024-05-07 23:10:31 +0530

    circular import error

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-
+from models import User, Post
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '206f66b5a40dc04517831de15f682596' # will be protecting us from attacks and ensure safe cookies
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+
+db = SQLAlchemy()
+db.init_app(app)
+app.app_context().push()
 
 
 blog_posts = [
